040af3aee0d08f751c8490f72c66c601d6c1b3cd
Dima Popovici <dimapopovici@gmail.com>
2015-10-23 01:53:58 +0000

    Changed new action in profiles controller

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -2,11 +2,13 @@ class ProfilesController < ApplicationController
   def new
    #form where a user can fill out their own profile.
     @user = User.find( params[:user_id] )
-    @profile = @user.build_profile
+    @profile = Profile.new
   end
   def create 
+    
     @user = User.find( params[:user_id] )
     @profile = @user.build_profile(profile_params)
+    
     if @profile.save
       flash[:success] = "Profile Updated!"
       redirect_to user_path( params[:user_id] )
